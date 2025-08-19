a0f02f64a064eb770a3d50dd1de1f2bd2e41ec7f
varshithapatnam226-maker <varshithapatnam226@gmail.com>
2025-08-19 16:21:40 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -1,21 +1,16 @@
 public class LinearSearch {
-
-    // Method to perform linear search
     public static int linearSearch(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
-                return i; // Return the index if found
+                return i; 
             }
         }
-        return -1; // Return -1 if not found
+        return -1; 
     }
-
     public static void main(String[] args) {
         int[] numbers = {5, 3, 8, 4, 2};
         int target = 4;
-
         int result = linearSearch(numbers, target);
-
         if (result == -1) {
             System.out.println(target + " not found in the array.");
         } else {
@@ -23,3 +18,4 @@ public class LinearSearch {
         }
     }
 }
+
